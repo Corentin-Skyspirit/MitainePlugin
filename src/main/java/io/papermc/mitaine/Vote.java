8b3b08f8888e4ac8fd9fb7619347c0aed488e5dd
Corentin Arnaud <corentin.arnaud45@gmail.com>
2024-02-08 22:43:57 +0100

    petit fix de vote

--- src/main/java/io/papermc/mitaine/Vote.java
+++ src/main/java/io/papermc/mitaine/Vote.java
@@ -12,7 +12,6 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.UUID;
-import java.util.regex.Pattern;
 
 public class Vote implements CommandExecutor {
 
@@ -20,7 +19,7 @@ public class Vote implements CommandExecutor {
     private static ArrayList<Integer> votes = new ArrayList<>();
     private static ArrayList<UUID> votants = new ArrayList<>();
     private static NamespacedKey cleBar = new NamespacedKey("vote", "vote");
-    private final static BossBar barVote = Bukkit.createBossBar(cleBar, "Un vote est en cours", BarColor.RED, BarStyle.SEGMENTED_10);
+    private final static BossBar barVote = Bukkit.createBossBar(cleBar, "Un vote est en cours faites §c/vote", BarColor.RED, BarStyle.SEGMENTED_20);
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
@@ -31,21 +30,48 @@ public class Vote implements CommandExecutor {
             if (cmd.getName().equalsIgnoreCase("creervote")) {
                 if (args.length > 1) {
                     StringBuilder bc = new StringBuilder();
-                    int i = 1;
                     votes.add(0);
-                    for (String part : args) {
-                        bc.append("- " + i + " pour " + part + "\n");
+                    for (String choix : args) {
+                        nb_choix++;
+                        bc.append("- §c" + nb_choix + "§f pour " + choix + "\n");
                         votes.add(0);
-                        i++;
                     }
-                    nb_choix = i - 1;
                     for (Player p : Bukkit.getOnlinePlayers()) {
                         p.sendMessage("Un vote est en cours, faites §c/vote§f pour voter.\n" + bc);
-                        barVote.addPlayer(p); // TODO Faire diminuer la bossbar
+                        barVote.addPlayer(p);
                     }
+                    barVote.setProgress(1.0);
                     return true;
                 } else {
                     player.sendMessage("§cLa commande est : /creervote <choix 1> ... <choix n>");
+                }
+            }
+
+            if (cmd.getName().equalsIgnoreCase("vote")) {
+
+                if (args.length == 1 && !votants.contains(player.getUniqueId())) {
+                    if (nb_choix == 0) {
+                        player.sendMessage("§cIl n'y a pas de vote en cours");
+                    } else {
+                        try {
+                            int choix = Integer.parseInt(args[0]);
+                            if (choix <= 0 || choix > nb_choix) {
+                                player.sendMessage("§cVous devez rentre un chiffre compris entre 1 et " + nb_choix);
+                            } else {
+                                votes.set(choix, votes.get(choix) + 1);
+                                votants.add(player.getUniqueId());
+                                player.sendMessage("Merci, vous avez voté : §c" + choix);
+                            }
+                        } catch (NumberFormatException e) {
+                            player.sendMessage("§cVous devez entrer un chiffre en paramètre");
+                        }
+                    }
+                } else {
+                    if (args.length != 1) {
+                        player.sendMessage("§cLa commande est : /vote <choix>");
+                    } else if (votants.contains(player.getUniqueId())) {
+                        player.sendMessage("§cVous ne pouvez pas voter plusieurs fois avec le même compte");
+                    }
                 }
             }
 
@@ -58,38 +84,26 @@ public class Vote implements CommandExecutor {
                         res.append("- §c" + votes.get(i) + "§f pour le vote " + i + "\n");
                     }
                     player.sendMessage("Le résultat du vote est :\n" + res);
+                    int idMax = 0;
+                    int sum = 0;
+                    for (int v = 0; v < votes.size(); v++) {
+                        sum += votes.get(v);
+                        if (votes.get(v) > votes.get(idMax)) {
+                            idMax = v;
+                        }
+                    }
+                    if (sum != 0) {
+                        String message = "Le choix §cn°" + idMax + "§f remporte le vote avec §c" + (double) 100 * votes.get(idMax) / sum + "%§f des voix";
+                        for (Player p : Bukkit.getOnlinePlayers()) {
+                            p.sendMessage(message);
+                        }
+                    } else {
+                        player.sendMessage("§c Il n'y a eu aucun vote");
+                    }
                     nb_choix = 0;
                     votes.clear();
                     votants.clear();
                     barVote.removeAll();
-                }
-            }
-
-            if (cmd.getName().equalsIgnoreCase("vote")) {
-
-                if (args.length == 1 && !votants.contains(player.getUniqueId())) {
-                    try {
-                        if (nb_choix == 0) {
-                            player.sendMessage("§cIl n'y a pas de vote en cours");
-                        } else {
-                            int choix = Integer.parseInt(args[0]);
-                            if (choix <= 0 || choix > nb_choix) {
-                                player.sendMessage("§cVous devez rentre un chiffre compris entre 1 et " + nb_choix);
-                            } else {
-                                votes.set(choix, votes.get(choix) + 1);
-                                votants.add(player.getUniqueId());
-                                player.sendMessage("Merci, vous avez voté : §c" + choix);
-                            }
-                        }
-                    } catch (NumberFormatException e) {
-                        player.sendMessage("§cVous devez entrer un chiffre en paramètre");
-                    }
-                } else {
-                    if (args.length != 1) {
-                        player.sendMessage("§cLa commande est : /vote <choix>");
-                    } else if (votants.contains(player.getUniqueId())) {
-                        player.sendMessage("§cVous ne pouvez pas voter plusieurs fois avec le même compte");
-                    }
                 }
             }
         }
